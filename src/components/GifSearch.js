a1f48250ecd0c0a9cd4815258daca05876f3e708
Ruth <ruro122020@gmail.com>
2023-11-12 12:17:11 -0500

    GifSearch component complete

--- src/components/GifSearch.js
+++ src/components/GifSearch.js
@@ -1,9 +1,24 @@
 import React, { useState } from 'react'
 
 const GifSearch = ({ onHanldeSubmit }) => {
-
+    const [searchInput, setSearchInput] = useState('')
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        onHanldeSubmit(searchInput)
+    }
     return (
-        <div></div>
+        <form onSubmit={handleSubmit}>
+            <label>
+                Enter a Search Term:
+                <input
+                    type='search'
+                    name='search'
+                    value={searchInput}
+                    onChange={(e) => setSearchInput(e.target.value)}
+                />
+            </label>
+            <button type='submit'>Find Gifs</button>
+        </form>
     )
 }
 
