95ddda59a8bdb642382a4eb510f2963d9e6c0ecd
Ruth <ruro122020@gmail.com>
2023-11-12 12:18:21 -0500

    fetch data from search input complete

--- src/components/GifListContainer.js
+++ src/components/GifListContainer.js
@@ -4,7 +4,9 @@ import GifSearch from './GifSearch'
 const GifListContainer = () => {
     const [gifList, setGifList] = useState([])
     const handleSearchSubmit = (searchValue) => {
-
+        fetch(`https://api.giphy.com/v1/gifs/search?q=${searchValue}n&api_key=${process.env.REACT_APP_KEY}&rating=g`)
+            .then(res => res.json())
+            .then(giph => setGifList(giph.data.slice(0, 3)))
     }
     return (
         <div>
